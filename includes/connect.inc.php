e57bde5e44ffd06bfd696eafa29e41440037748e
willhodgson <willhodgson2004@gmail.com>
2017-09-04 13:19:47 -0500

    first fully functioning, aside from login and register

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -14,15 +14,17 @@
 
 
 <?php
-
+   //error_reporting(E_ALL ^ E_WARNING);
 	$mysql_host = 'localhost';
 	$mysql_user = 'willhodg_user1';
 	$mysql_pass = 'Maple1132';
 	
 	$mysql_db = 'willhodg_main_db';
 	
-	$connect = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
-	
+	$connect = @mysql_connect($mysql_host, $mysql_user, $mysql_pass);
+	//$connect = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+   //$result = $db->query("INSERT INTO table(firstname, lastname) VAULES('John', 'Doe')");
+   
 	if(!$connect){die('Could not connect to database');}
 	
 	mysql_select_db($mysql_db, $connect);
